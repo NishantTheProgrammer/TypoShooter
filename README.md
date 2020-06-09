21fd1d66dca0f5c1132f262d953e54de6e02a9b7
NishantTheProgrammer <knishant113@gmail.com>
2020-06-09 20:27:45 +0530

    User interface done

---
+++ README.md
@@ -0,0 +1,4 @@
+Not For mobiles
+for best experiene view in 
+1080x1920
+hosted on https://nishanttheprogrammer.github.io/TypoShooter/
--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 new Vue({
     el: '#app',
     data: {
-        name: "Nishant"
+        lives: 3,
+        score: 230,
+        time: 28
     },
     methods: {
 
