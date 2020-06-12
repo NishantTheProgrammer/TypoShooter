4c737b62260a088df019b45fa855aaacae872df8
NishantTheProgrammer <knishant113@gmail.com>
2020-06-12 22:10:43 +0530

    Almost done but still have some bugs

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-
+var countDown;
 
 new Vue({
     el: '#app',
     data: {
+        
         stage: 0,
         lives: 4,
         score: 0,
@@ -15,6 +16,8 @@ new Vue({
         addPoint: false,
         points: 0,
         infoPanel: false,
+        gameOver: false,
+        isStart: false,
 
 
 
@@ -22,42 +25,81 @@ new Vue({
 
         slip: new Audio('./assets/sound/slip.mp3'),
         shoot: new Audio('./assets/sound/shoot.mp3'),
-        startingMusic: new Audio('./assets/sound/starting.mp3')
+        startingMusic: new Audio('./assets/sound/starting.mp3'),
+        failMusic: new Audio('./assets/sound/fail.wav'),
+        successMusic: new Audio('./assets/sound/success.mp3')
     },
     methods: {
         commander: function(event)
         {
+
             const command = event.target.value.toLowerCase().split(" ");
             event.target.value = "";
 
             this.activity++;        //to fix issue of not updating style
             switch(command[0])
             {
+                case "restart":
+                    if(this.gameOver)
+                    {
+                        clearInterval(countDown);
+                        this.stage= 0;
+                        this.lives= 4;
+                        this.score= 0;
+                        this.time= 0;
+                        this.color= 'blue';
+                        this.archery= [400, 200];   // x, y cordinate;
+                        this.crosshair= [450, 250];     // x, y cordinate;
+                        this.activity= 0;
+                        this.shootImg= 0;
+                        this.addPoint= false;
+                        this.points= 0;
+                        this.infoPanel= false;
+                        this.gameOver= false;
+                        this.isStart= false;
+                    }
+                    
+                    break;
                 //----------------------------------------------------Start Command-----------------------------------------------------          
                 case "start":
-                    this.stage++;
-                    this.startingMusic.play();
-                    this.time = 30;
-                    let countDown = setInterval(()=>{
-                        if(this.time == 1)
+                    if(!this.isStart)
+                    {
+                        this.isStart = true;
+
+                        if(this.stage < 10)
                         {
-                            this.lives--;
-                            clearInterval(countDown);
+                            this.time = 30 + ( 30 - this.stage * 5);        // each stage time will start with -5s 
                         }
-                        this.time--;
-                    }, 1000)
-                    this.archery[0] = Math.floor((Math.random() * 800));            // 1000 - 200
-                    this.archery[1] = Math.floor((Math.random() * 400));            // 600 - 200
-                    
-                    this.crosshair[0] = Math.floor((Math.random() * 900));            // 1000 - 200
-                    this.crosshair[1] = Math.floor((Math.random() * 500));            // 600 - 200
-                 break;
+                        else
+                        {
+                            this.time = 15;
+                        }
+                        this.stage++;
+                        this.startingMusic.play();
+                            countDown = setInterval(()=>{
+                            this.time--;
+                            if(this.time == 1)
+                            {
+                                this.nextStage(false);
+                            }
+                        }, 1000)
+                        this.archery[0] = Math.floor((Math.random() * 800));            // 1000 - 200
+                        this.archery[1] = Math.floor((Math.random() * 400));            // 600 - 200
+                        
+                        this.crosshair[0] = Math.floor((Math.random() * 900));            // 1000 - 200
+                        this.crosshair[1] = Math.floor((Math.random() * 500));            // 600 - 200
+                    }
+                    break;
 
 
                 //----------------------------------------------------Left Command------------------------------------------------------          
                 case "left":
                     this.slip.play();
                     this.crosshair[0] -= Number(command[1]);
+                    if(this.crosshair[0] < 0)
+                    {
+                        this.crosshair[0] = -20;
+                    }
 
                  break;
 
@@ -66,6 +108,10 @@ new Vue({
                 case "right":
                     this.slip.play();
                     this.crosshair[0] += Number(command[1]);
+                    if(this.crosshair[0] > 900)
+                    {
+                        this.crosshair[0] = 900 + 20;
+                    }
 
                  break;
 
@@ -74,6 +120,10 @@ new Vue({
                 case "up":
                     this.slip.play();
                     this.crosshair[1] -= Number(command[1]);
+                    if(this.crosshair[1] < 0)
+                    {
+                        this.crosshair[1] = -20;
+                    }
 
                  break;
 
@@ -82,6 +132,11 @@ new Vue({
                 case "down":
                     this.slip.play();
                     this.crosshair[1] += Number(command[1]);
+                    if(this.crosshair[1] > 500)
+                    {
+                        this.crosshair[1] = 500 + 20;
+                    }
+                    
 
                  break;
 
@@ -100,11 +155,12 @@ new Vue({
                             this.score += this.points;
                             this.addPoint = false;
                             this.points = 0;
+                            this.nextStage(true);
                         }, 1000)
                     }
                     else                                // shoot outside the archery
                     {
-                        console.log("Very bad");
+                        this.nextStage(false);
                     }
 
                  break;
@@ -120,6 +176,46 @@ new Vue({
 
             actualDistance = Math.sqrt((distance[0] * distance[0]) + (distance[1] * distance[1]))       // Pythagoras Theory
             return actualDistance;            
+        },
+        nextStage: function(success)
+        {
+            this.shootImg = 0;
+            if(this.lives < 1)
+            {
+                clearInterval(countDown);
+                this.time = 0;
+                this.gameOver = true;
+            }
+            else
+            {
+
+                this.stage++;
+                if(success)
+                {
+                    this.startingMusic.play();
+                }
+                else
+                {
+                    this.lives--;
+                    this.failMusic.play();
+                }
+               
+                if(this.stage < 9)
+                {
+                    this.time = 30 + ( 30 - this.stage * 5);   // each stage time will start with -5s 
+                }
+                else
+                {
+                    this.time = 15;
+                }
+
+
+                this.archery[0] = Math.floor((Math.random() * 800));            // 1000 - 200
+                this.archery[1] = Math.floor((Math.random() * 400));            // 600 - 200
+                
+                this.crosshair[0] = Math.floor((Math.random() * 900));            // 1000 - 200
+                this.crosshair[1] = Math.floor((Math.random() * 500));            // 600 - 200
+            }
         }
     }
 });
